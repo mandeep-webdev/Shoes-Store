349822c080d2999425fb29ea5ccb76060ded2c88
Mandeep Kaur <mandeep.fullstack.dev@gmail.com>
2023-05-02 13:24:13 +0530

    initial commit

---
+++ components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const [showCatMenu, setShowCatMenu] = useState(false);
+  const [show, setShow] = useState('translate-y-0');
+  const [lastScrollY, setLastScrollY] = useState(0);
+  return <div className="text">Header</div>;
+};
+
+export default Header;
